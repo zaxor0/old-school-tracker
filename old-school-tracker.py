32630c259c7ae50e18aa601018964ba751e0bc36
Zachary Watts <zaxor0@gmail.com>
2025-11-17 22:37:36 -0500

    working on tracked objects

--- old-school-tracker.py
+++ old-school-tracker.py
@@ -10,6 +10,7 @@ with open('encounters.yaml','r') as f:
 class Trackable():
     def __init__(self,kind):
         # kinds should be one of torch, latern, spell
+        self.active = True
         self.kind = kind
         self.turns_passed = -1 # need to fix this so that turns dont increment when torches are lit
         self.total_turns = self.set_turns()
@@ -22,10 +23,19 @@ class Trackable():
         }
         return turn_lifespan[self.kind]
 
+    def update_turns(self):
+        passed = self.turns_passed + 1
+        if self.total_turns - passed <= 1:
+            self.active = False
+        if self.active:
+            self.turns_passed += 1
+
     def print_meter(self):
-        turns_left = self.total_turns - (self.turns_passed)
-        meter = '[' + (self.turns_passed * '.') + (turns_left * '|') +']'
-        return f"{self.kind}: {meter}"
+        if self.active:
+            passed = self.turns_passed + 1
+            turns_left = self.total_turns - passed
+            meter = '[' + (passed * '.') + (turns_left * '|') +']'
+            print(f"{self.kind}: {meter} \t {turns_left} of {self.total_turns} remaining")
 
 # umbrella to hold all the objects and time passed
 class Session():
@@ -33,6 +43,7 @@ class Session():
         self.time_passed = "0m"
         self.turns = 1
         self.rolls = []
+        self.encounters = []
         self.tracked_objects = []
 
     def update_time(self):
@@ -45,7 +56,14 @@ class Session():
         else:
             self.time_passed = f"{hours}h {minutes}m"
         for to in self.tracked_objects:
-            to.turns_passed += 1
+            to.update_turns()
+
+    def spent_torches(self):
+        st = 0
+        for to in self.tracked_objects:
+            if to.kind == "torch" and to.active == False:
+                st += 1
+        return st
 
     def roll_dice(self):
         dice = input('What would you like to roll, in 1d6 format? ')
@@ -62,8 +80,9 @@ class UserInterface():
         self.keys = {
             'd' : '[d]ice roll',
             't' : '[t]urn passed',
-            'n' : '[n]ew torch',
+            'n' : '[n]ew light source',
             'e' : '[e]ncounter check',
+            's' : '[s]pell',
             'q' : '[q]uit'
         }
 
@@ -73,10 +92,12 @@ class UserInterface():
         print(title_box)
         print(f"Time passed:  {new_game.time_passed}")
         print(f"Current turn: {new_game.turns}")
+        print(f"Spent Torches: {new_game.spent_torches()}")
         if new_game.tracked_objects:
             for to in new_game.tracked_objects:
-                print(to.print_meter())
-        print(f"Rolls made: {new_game.rolls}")
+                to.print_meter()
+        print(f"\nEncounters: {new_game.encounters}")
+        print(f"\nRolls made: {new_game.rolls}")
         if new_game.rolls:
             print(f"Last roll: {new_game.rolls[-1]}")
         else:
@@ -115,16 +136,22 @@ def main():
     new_game = Session()
     ui = UserInterface()
     while True:
-        new_game.update_time()
         ui.main_screen(new_game)
         key = ui.user_input()
         if key == 't':
             new_game.turns += 1
+            new_game.update_time()
         if key == 'd':
             new_game.roll_dice()
         if key == 'n':
-            torch = Trackable("torch")
-            new_game.tracked_objects.append(torch)
+            kind = input("Are you lighting a [t]orch or a [l]antern? ")
+            if kind in ['t','l']:
+                if kind == 'l':
+                    kind = 'lantern'
+                if kind == 't':
+                    kind = 'torch'
+                light_source= Trackable(kind)
+                new_game.tracked_objects.append(light_source)
         if key == 'e':
             print("Encounter Tables:")
             tables = list(encounter_table.keys())
@@ -136,10 +163,12 @@ def main():
             monster = list(encounter.keys())[0]
             dice = encounter[monster]
             dice = dice_roller(int(dice.split('d')[0]), int(dice.split('d')[1]))
-            input(f"{dice}, {monster}")
+            new_game.encounters.append(f"{dice} {monster}")
         if key == 'q':
             q = input('Are you sure you want to quit? ')
             if q.lower() in ['y', 'ye', 'yes', 'ya', 'yup', 'yeah']:
+                quit()
+        if key == 'qy':
                 quit()
 
 main()
